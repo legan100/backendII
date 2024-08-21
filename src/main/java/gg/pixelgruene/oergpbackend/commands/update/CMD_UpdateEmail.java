d2477700c31dc2320f0a1ecce1936a6359d2594a
legan100 <niconagel@outlook.de>
2024-08-21 16:51:45 +0200

    chore: backup

--- src/main/java/gg/pixelgruene/oergpbackend/commands/update/CMD_UpdateEmail.java
+++ src/main/java/gg/pixelgruene/oergpbackend/commands/update/CMD_UpdateEmail.java
@@ -1,6 +1,25 @@
 package gg.pixelgruene.oergpbackend.commands.update;
 
+import gg.pixelgruene.oergpbackend.Main;
+import gg.pixelgruene.oergpbackend.user.User;
+import gg.pixelgruene.oergpbackend.user.UserController;
+
 public class CMD_UpdateEmail {
 
+    public static void onCommand(){
+        User user = new User();
+        UserController userController = new UserController();
+        Main.getLogger().logInfo("What is the name of the user who should receive a new email?");
+        String username;
+        username = Main.getScanner().nextLine();
+        Main.getLogger().logInfo("What is the new email address of the user?");
+        String newEmail = Main.getScanner().nextLine();
+        System.out.println(user.isUserExists(username));
+        if(user.isUserExists(username)) {
+            userController.updateEmail(username, newEmail);
+        }else{
+            Main.getLogger().logInfo("the User " + username + " don't exist.");
+        }
+    }
 
 }
