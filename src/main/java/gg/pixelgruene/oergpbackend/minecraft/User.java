a6a638198fe3d69727b6ffaf29e6dbd3f0272369
legan100 <niconagel@outlook.de>
2024-08-25 11:20:55 +0200

    chore: backup

--- src/main/java/gg/pixelgruene/oergpbackend/minecraft/User.java
+++ src/main/java/gg/pixelgruene/oergpbackend/minecraft/User.java
@@ -1,18 +1,10 @@
 package gg.pixelgruene.oergpbackend.minecraft;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import gg.pixelgruene.oergpbackend.Main;
 import org.bukkit.entity.Player;
 import org.springframework.stereotype.Service;
 
-import java.io.File;
-import java.io.IOException;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.List;
+import java.sql.*;
 
 @Service
 public class User {
@@ -22,10 +14,8 @@ public class User {
     public int getPlayerMoneyByUsername(String username) {
         String query = "SELECT money FROM moneyTable WHERE username = ?";
         int money = 0;
-
         try (Connection connection = Main.getMoney().getConnection();  // Stelle sicher, dass du eine funktionierende Verbindung erhältst
-             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
-
+            PreparedStatement preparedStatement = connection.prepareStatement(query)) {
             preparedStatement.setString(1, username);
             ResultSet resultSet = preparedStatement.executeQuery();
 
@@ -35,11 +25,9 @@ public class User {
                 // Wenn keine Ergebnisse gefunden wurden
                 System.out.println("Spieler mit dem Benutzernamen " + username + " wurde nicht gefunden.");
             }
-
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
         return money;
     }
 
@@ -48,8 +36,7 @@ public class User {
         int money = 0;
 
         try (Connection connection = Main.getMoney().getConnection();  // Stelle sicher, dass du eine funktionierende Verbindung erhältst
-             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
-
+            PreparedStatement preparedStatement = connection.prepareStatement(query)) {
             preparedStatement.setString(1, playerUUID);
             ResultSet resultSet = preparedStatement.executeQuery();
 
@@ -59,30 +46,32 @@ public class User {
                 // Wenn keine Ergebnisse gefunden wurden
                 System.out.println("Spieler mit der UUID " + playerUUID + " wurde nicht gefunden.");
             }
-
         } catch (SQLException e) {
             e.printStackTrace();
             // Hier kannst du auch mit `throw new RuntimeException(e);` weiterleiten oder eine eigene Exception werfen.
         }
-
         return money;
     }
 
-    public String getFirstJoin(String p) {
-        ObjectMapper mapper = new ObjectMapper();
-        try {
-            // Lese die JSON-Datei und konvertiere sie in eine Liste von User-Objekten
-            List<User> users = mapper.readValue(new File("/home/nico/Minecraftserver/Lobby/usercache.json"), new TypeReference<>() {
-            });
+    private Connection getConnection() {
+        return Main.getBackend().getConnection();
+    }
 
-            // Ausgabe der Benutzerdaten
-            for (User user : users) {
-                System.out.println(user);
+    public String getFirstJoin(String uuid) {
+        String query = "SELECT first_join FROM player_data WHERE UUID = ?";
+        String firstJoin = null;
+
+        try (Connection connection = Main.getBackend().getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.setString(1, uuid);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                firstJoin = resultSet.getString("first_join");
             }
-        } catch (IOException e) {
+        } catch (SQLException e) {
             e.printStackTrace();
         }
-        return p;
+        return firstJoin;
     }
 
 }
